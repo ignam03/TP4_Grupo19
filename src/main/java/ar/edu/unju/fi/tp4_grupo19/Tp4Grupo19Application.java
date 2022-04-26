b6fcb36f76dda870b3b9c83beb929d383ba505ad
ignam03 <ignafree97@gmail.com>
2022-04-26 14:36:35 -0300

    project grupo-19

---
+++ src/main/java/ar/edu/unju/fi/tp4_grupo19/Tp4Grupo19Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp4_grupo19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo19Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/fi/tp4_grupo19/Tp4Grupo19ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp4_grupo19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4Grupo19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
